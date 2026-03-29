c5ef8901b3805e2a6ddd781eefe9a58266981002
bsic719 <besic719@gmail.com>
2026-03-29 19:32:49 -0400

    leafnode class and test suite created

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -28,3 +28,21 @@ class HTMLNode():
                self.children == other.children and
                self.props == other.props
           )
+
+class LeafNode(HTMLNode):
+     def __init__(self, tag=None, value=None, props=None):
+          super().__init__(tag, value, None, props)
+     
+     def to_html(self):
+          if self.value == None:
+               raise ValueError
+          if self.tag == None:
+               return self.value
+          
+          if self.props == None:
+               return f"<{self.tag}>{self.value}</{self.tag}>"
+          else:
+               return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+     
+     def __repr__(self):
+          return f"LeafNode({self.tag}, {self.value}, {self.props})"
---
+++ src/test_leafnode.py
@@ -0,0 +1,22 @@
+import unittest
+
+from htmlnode import HTMLNode, LeafNode
+
+class TestLeafNode(unittest.TestCase):
+     def test_leaf_to_html_p(self):
+          node = LeafNode("p", "This is a paragraph of text.")
+          self.assertEqual(
+               node.to_html(), "<p>This is a paragraph of text.</p>"
+          )
+     
+     def test_leaf_to_html_a(self):
+          node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+          self.assertEqual(
+               node.to_html(), '<a href="https://www.google.com">Click me!</a>'
+          )
+
+     def test_repr(self):
+          node = LeafNode("p", "Hello, world!")
+          self.assertEqual(
+               repr(node), "LeafNode(p, Hello, world!, None)"
+          )
